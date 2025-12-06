6b966369d5b955c0612e8fe4ee0ca1664f6f6a9f
voldpix <vold.zavada@gmail.com>
2025-12-06 12:47:40 -0500

    init

--- src/main/java/app/voldpix/mockboard/MockBoardApp.java
+++ src/main/java/app/voldpix/mockboard/MockBoardApp.java
@@ -1,13 +1,8 @@
 package app.voldpix.mockboard;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class MockBoardApp {
+
+    public static void main(String[] args) {
+
     }
 }
